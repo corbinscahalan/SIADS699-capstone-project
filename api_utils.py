4a6f576dda8eaa9ac0803de137af0a84e085915d
MarlonShakespeare <jeffols@umich.edu>
2022-10-15 21:00:05 -0500

    New extraction function

--- api_utils.py
+++ api_utils.py
@@ -1,5 +1,15 @@
-import googleapiclient.discovery
+###################
+##
+##  YouTube API utility functions
+##
+##  SIADS 699 Capstone
+##
+##  C. Cahalan, A. Levin-Koopman, J. Olson
+##
+####################
 
+
+import googleapiclient.discovery
 from typing import List, Set, Dict, Tuple, Optional
 
 def make_client(api_key: str) -> object:
@@ -59,6 +69,14 @@ def get_uploads(yt_client: object, channel_id: str, max_vids: int = 50) -> List[
 
 def get_channel_from_vid(yt_client: object, vid_id: str) -> str:
 
+    # Finds the channel associated with the input video
+
+    # Parameters:
+    # yt_client -- YouTube API client for requests
+    # vid_id -- ID of a YouTube video
+
+    # Returns:
+    # ID string of channel associated with video
 
     results = yt_client.videos().list(
         part = 'snippet',
@@ -69,3 +87,84 @@ def get_channel_from_vid(yt_client: object, vid_id: str) -> str:
 
     return channel_id
     
+
+
+def extract_by_query(yt_client: object, query: str, max_channels: int = 5, max_vids: int = 5) -> object:
+
+    # Performs data extraction from YouTube API using a keyword(s) query
+
+    # Parameters:
+    # yt_client -- YouTube API client for requests
+    # query -- A string of key words, presumably related to culinary topics
+    # max_channels -- the number of channels to survey              -->> DO NOT CHANGE FROM DEFAULT VALUE IN CURRENT BUILD
+    # max_vids -- the number of videos to pull from each channel    -->> DO NOT CHANGE FROM DEFAULT VALUE IN CURRENT BUILD
+
+    # Returns:
+    # Pandas dataframe with channel and video features
+
+    chan_cols = [ 'chan_id', 'chan_name', 'chan_viewcount', 'chan_subcount', 'chan_start_dt', 'chan_thumb', 'chan_vidcount']
+    vid_cols = ['vid_id', 'vid_name', 'vid_publish_dt', 'vid_thumb', 'vid_duration', 'vid_caption', 'vid_viewcount']#, 'vid_likecount', 'vid_commentcount']
+
+    df = pd.DataFrame(columns = chan_cols + vid_cols)
+
+    channel_results = yt_client.search().list(
+        part = 'snippet',
+        type = 'channel',
+        q = query + ' cooking videos',
+        maxResults = max_channels
+    ).execute()
+
+    # 100 quota for the search
+
+    for channel in channel_results['items']:
+        channel_id = channel['id']['channelId']
+
+        chan_info = yt_client.channels().list(
+            part = ['snippet', 'contentDetails', 'statistics', 'topicDetails'],
+            id = channel_id
+        ).execute()['items'][0]
+
+        # 1 quota x 50 channels = 50 quota
+
+        chan_snip = chan_info['snippet']
+        chan_det = chan_info['contentDetails']
+        chan_stats = chan_info['statistics']
+
+        # Building dataframe rows, starting with channel features.
+
+        chan_values = [ channel_id, chan_snip['title'], chan_stats['viewCount'], chan_stats['subscriberCount'], chan_snip['publishedAt'], chan_snip['thumbnails']['default']['url'], chan_stats['videoCount'] ]
+
+        chan_uploads_id = chan_det['relatedPlaylists']['uploads']
+
+        # Up to 10 quota x 50 channels <= 500 quota
+
+        # Get the id values for the channel's vids
+
+        vid_ids = get_uploads(yt_client, channel_id, max_vids)
+
+        for vid_id in vid_ids:
+
+            vid_info = yt_client.videos().list(
+                part = ['contentDetails', 'snippet', 'statistics'],
+                id = vid_id
+            ).execute()['items'][0]
+
+            # 1 quota x 50 channels x 500 videos = 25000 quota
+
+            vid_snip = vid_info['snippet']
+            vid_det = vid_info['contentDetails']
+            vid_stats = vid_info['statistics']
+
+            # Finish building rows, add to dataframe
+
+            vid_values = [ vid_id, vid_snip['title'], vid_snip['publishedAt'], vid_snip['thumbnails']['default']['url'], 
+                            vid_det['duration'], vid_det['caption'], vid_stats['viewCount']] #, vid_stats['likeCount'], vid_stats['commentCount']]
+
+            current_row = len(df.index)+1
+
+            df.loc[current_row,:] = chan_values + vid_values
+
+    return df
+
+
+
